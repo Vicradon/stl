583876211cff40bbff843026b6c3da078c0c6a14
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-05-02 00:20:28 +0100

    Dunno

--- js/index.js
+++ js/index.js
@@ -4,8 +4,10 @@ const form = $('#text-to-send-form');
 form.onsubmit = e => {
   e.preventDefault();
   const text = form['text-to-send'].value;
-console.log(text)
-  fetch('/', {
+
+  fetch('/text-to-send/?' + new URLSearchParams({
+    message: text
+  }), {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -13,5 +15,15 @@ console.log(text)
     body: JSON.stringify({
       message: text
     })
-  });
+  })
+  //.then(res => res.json())
+  //   .then(data => console.log(data))
+  //   .catch(err => console.error(err))
 }
+
+fetch('.netlify.com/.netlify/functions/text-to-send', {
+  method: "Get",
+  headers: {
+    'Content-Type': 'text/plain'
+  }
+}).then(res => res.json()).then(data => $('.newMessage').textContent = data).catch(console.error);
